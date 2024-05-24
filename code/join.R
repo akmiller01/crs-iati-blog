a442a03ecf5a5860f962a9318b82ed7b5485b4ef
akmiller01 <alex.k.miller@gmail.com>
2024-05-24 13:29:31 -0400

    Sector/Recipient

---
+++ code/join.R
@@ -0,0 +1,87 @@
+#### Setup ####
+list.of.packages <- c("rstudioapi", "dplyr", "data.table", "countrycode", "stringr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+wd <- dirname(getActiveDocumentContext()$path) 
+setwd(wd)
+setwd("../")
+#### End setup ####
+
+crs = fread("large_data/crs.csv")
+iati = fread("large_data/iati_24052024.csv")
+
+# Correction for missing reporting_org_ref, and nonbreaking space
+iati$reporting_org_ref[which(startsWith(iati$iati_identifier,"GB-CHC-294329"))] = "GB-CHC-294329"
+iati$reporting_org_ref[which(startsWith(iati$iati_identifier,"FR-6"))] = "FR-6"
+iati$reporting_org_ref = str_trim(iati$reporting_org_ref)
+
+oecd_donor_mapping = fread('data/iati_donor_mapping.csv')
+oecd_donor_mapping = subset(oecd_donor_mapping, !is.na(`Donor code`))
+# Account for splitting
+iati$index = c(1:nrow(iati))
+pre_sum = sum(iati$x_transaction_value_usd, na.rm=T)
+iati = merge(iati, oecd_donor_mapping, by="reporting_org_ref", all.x = T, allow.cartesian=T)
+duplicated_indicies = iati$index[which(duplicated(iati$index))]
+duplicated_counts = data.table(table(duplicated_indicies))
+names(duplicated_counts) = c("index", "count")
+duplicated_counts$index = as.numeric(duplicated_counts$index)
+duplicated_counts$count = duplicated_counts$count + 1
+duplicated_counts$value_multiplier = 1 / duplicated_counts$count 
+duplicated_counts$count = NULL
+iati = merge(iati, duplicated_counts, by="index", all.x=T)
+iati$value_multiplier[which(is.na(iati$value_multiplier))] = 1
+iati$x_transaction_value_usd = iati$x_transaction_value_usd * iati$value_multiplier
+iati$index = NULL
+iati$value_multiplier = NULL
+post_sum = sum(iati$x_transaction_value_usd, na.rm=T)
+stopifnot(format(pre_sum, scientific=F) == format(post_sum, scientific=F))
+
+# Correction for World Bank publisher mapping to IDA Donor Code
+wb = subset(iati, reporting_org_ref=="44000")
+wb = subset(wb, grepl("International Development Association", funding_orgs))
+nonwb = subset(iati, reporting_org_ref!="44000")
+iati = rbind(wb, nonwb)
+
+# Remove non-core funding for FAO
+fao = subset(iati, reporting_org_ref=="XM-DAC-41301")
+fao = subset(fao, grepl("Food and Agriculture Organization (FAO)", funding_orgs, fixed=T))
+nonfao = subset(iati, reporting_org_ref!="XM-DAC-41301")
+iati = rbind(fao, nonfao)
+
+# Fix scale of IADB
+iadb = subset(iati, reporting_org_ref=="XI-IATI-IADB")
+iadb$x_transaction_value_usd = iadb$x_transaction_value_usd / 1000
+noniadb = subset(iati, reporting_org_ref!="XI-IATI-IADB")
+iati = rbind(iadb, noniadb)
+
+iati = subset(iati, !is.na(`Donor code`))
+iati$sector_code = substr(iati$x_sector_code, 1, 3)
+iati$x_transaction_value_usd = iati$x_transaction_value_usd / 1000000
+iati_agg = data.table(iati)[,.(usd_disbursement_iati=sum(x_transaction_value_usd, na.rm=T)),by=.(
+  `Donor code`, `Donor name`, x_transaction_year, x_recipient_code, sector_code
+)]
+names(iati_agg) = c("donor_code", "donor_name" ,"year", "recipient_iso2_code", "sector_code", "usd_disbursement_iati")
+iati_agg$recipient_iso3_code = countrycode(
+  iati_agg$recipient_iso2_code, 
+  origin="iso2c", 
+  destination="iso3c"
+)
+iati_agg = subset(iati_agg, !is.na(recipient_iso3_code))
+iati_agg$recipient_iso2_code = NULL
+iati_agg$sector_code = as.character(iati_agg$sector_code)
+
+crs_agg = data.table(crs)[,.(usd_disbursement_crs=sum(usd_disbursement, na.rm=T)),by=.(
+  donor_code, donor_name, year, recipient_iso3_code, sector_code
+)]
+crs_agg$sector_code = as.character(crs_agg$sector_code)
+
+dat = merge(crs_agg, iati_agg, all=T)
+dat$usd_disbursement_crs[which(is.na(dat$usd_disbursement_crs))] = 0
+dat$usd_disbursement_iati[which(is.na(dat$usd_disbursement_iati))] = 0
+
+dat$recipient_name = countrycode(dat$recipient_iso3_code, origin="iso3c", destination="country.name")
+dat = subset(dat, !is.na(recipient_name))
+
+fwrite(dat, "data/merged_crs_iati.csv")
---
+++ code/model.R
@@ -0,0 +1,72 @@
+#### Setup ####
+list.of.packages <- c("rstudioapi", "data.table")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+wd <- dirname(getActiveDocumentContext()$path) 
+setwd(wd)
+setwd("../")
+
+rmse = function(vec1, vec2){
+  sqrt(mean((vec1 - vec2)^2, na.rm=T))
+}
+#### End setup ####
+
+dat = fread("data/merged_crs_iati.csv")
+dat = dat[,.(
+  usd_disbursement_crs=sum(usd_disbursement_crs),
+  usd_disbursement_iati=sum(usd_disbursement_iati)
+),
+by=.(year, sector_code, recipient_name, recipient_iso3_code)]
+dat = dat[order(dat$recipient_name, dat$sector_code, dat$year)]
+dat[,"usd_disbursement_crs_t1":=shift(usd_disbursement_crs),by=.(recipient_name, sector_code)]
+dat[,"usd_disbursement_iati_t1":=shift(usd_disbursement_iati),by=.(recipient_name, sector_code)]
+
+dat$delta_iati = (dat$usd_disbursement_iati - dat$usd_disbursement_iati_t1)
+
+dat$sector_code = factor(dat$sector_code)
+
+dat_train = subset(dat, year < 2022)
+dat_test = subset(dat, year == 2022)
+
+fit = lm(
+  usd_disbursement_crs~ # CRS this year is a function of
+    # Constant alpha
+    usd_disbursement_iati+ # plus beta0 * IATI this year
+    usd_disbursement_crs_t1+ # plus beta1 * CRS last year
+    delta_iati+ # plus beta2 * the absolute change in IATI from last year
+    sector_code # plus sector fixed effects
+    , data=dat_train)
+summary(fit)
+confidence = predict.lm(fit, newdata = dat_test, interval = "confidence")
+dat_test$usd_disbursement_iati_fit = confidence[,1]
+dat_test$usd_disbursement_iati_lwr = confidence[,2]
+dat_test$usd_disbursement_iati_upr = confidence[,3]
+plot(usd_disbursement_crs~usd_disbursement_iati, data=dat_test)
+abline(0,1)
+original_rmse = rmse(dat_test$usd_disbursement_crs, dat_test$usd_disbursement_iati)
+original_rmse
+plot(usd_disbursement_crs~usd_disbursement_iati_fit, data=dat_test)
+abline(0,1)
+fit_rmse = rmse(dat_test$usd_disbursement_crs, dat_test$usd_disbursement_iati_fit)
+fit_rmse
+
+
+dat_train = subset(dat, year < 2023)
+dat_predict = subset(dat, year < 2024)
+fit = lm(
+  usd_disbursement_crs~ # CRS this year is a function of
+    # Constant alpha
+    usd_disbursement_iati+ # plus beta0 * IATI this year
+    usd_disbursement_crs_t1+ # plus beta1 * CRS last year
+    delta_iati+ # plus beta2 * the absolute change in IATI from last year
+    sector_code # plus sector fixed effects
+  , data=dat_train)
+summary(fit)
+confidence = predict.lm(fit, newdata = dat_predict, interval = "confidence")
+dat_predict$usd_disbursement_crs_fit = confidence[,1]
+dat_predict$usd_disbursement_crs_lwr = confidence[,2]
+dat_predict$usd_disbursement_crs_upr = confidence[,3]
+dat_predict$usd_disbursement_crs[which(dat_predict$year==2023)] = dat_predict$usd_disbursement_crs_fit[which(dat_predict$year==2023)]
+fwrite(dat_predict, "data/modeled_crs_iati.csv")
